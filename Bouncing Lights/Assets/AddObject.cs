ea220d6ac91dd959332b17eabc45d91a275d1a89
Roger Cheng <roger.random@outlook.com>
2021-01-26 20:31:02 -0800

    Random position generated at regular time interval

---
+++ Bouncing Lights/Assets/AddObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObject : MonoBehaviour
+{
+    public GameObject objectToAdd;
+    public float timeInterval;
+
+    private float timeSeconds;
+    private float timeLastGen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeSeconds = 0;
+        timeLastGen = 0;
+        Add();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSeconds += Time.deltaTime;
+
+        if (timeSeconds > timeLastGen + timeInterval)
+        {
+            Add();
+        }
+    }
+
+    public void Add()
+    {
+        Vector3 randomPosition = new Vector3(
+            Random.Range(-9f, 9f),
+            7f,
+            Random.Range(-9f, 9f));
+
+        Instantiate(objectToAdd, randomPosition, Quaternion.identity);
+
+        timeLastGen = timeSeconds;
+    }
+}
