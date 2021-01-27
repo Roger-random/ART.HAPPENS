b8aa93490d42a373e9f10b12ef1c4ff38cde66e0
Roger Cheng <roger.random@outlook.com>
2021-01-26 19:55:28 -0800

    Bouncy box

---
+++ Bouncing Lights/Assets/DestroyDropped.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyDropped : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform objTransform = gameObject.transform;
+
+        // Once we drop below -10, remove from scene.
+        if (objTransform.position.y < -10)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+}
