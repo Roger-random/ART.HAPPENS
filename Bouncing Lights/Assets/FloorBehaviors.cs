f0861977206321ea4872c905ee7df02b3d703ff3
Roger Cheng <roger.random@outlook.com>
2021-01-27 23:08:37 -0800

    Space for more cubes

---
+++ Bouncing Lights/Assets/FloorBehaviors.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FloorBehaviors : MonoBehaviour
+{
+    public InputActionAsset playerControls;
+    public InputAction fireAction;
+    public AddObject refAddObject;
+
+    public void Awake()
+    {
+        InputActionMap inputActionMap = playerControls.FindActionMap("Player");
+
+        fireAction = inputActionMap.FindAction("Fire");
+        fireAction.performed += FireAction_performed;
+    }
+
+    private void FireAction_performed(InputAction.CallbackContext obj)
+    {
+        refAddObject.Add();
+    }
+
+    public void OnEnable()
+    {
+        fireAction.Enable();
+    }
+
+    public void OnDisable()
+    {
+        fireAction.Disable();
+    }
+}
