70fe572a97cdc459a6e01ed26014ce61127dd7f4
Roger Cheng <roger.random@outlook.com>
2021-01-28 00:23:10 -0800

    Platform tilt

--- Bouncing Lights/Assets/FloorBehaviors.cs
+++ Bouncing Lights/Assets/FloorBehaviors.cs
@@ -7,6 +7,7 @@ public class FloorBehaviors : MonoBehaviour
 {
     public InputActionAsset playerControls;
     public InputAction fireAction;
+    public InputAction tiltAction;
     public AddObject refAddObject;
 
     public void Awake()
@@ -15,6 +16,31 @@ public class FloorBehaviors : MonoBehaviour
 
         fireAction = inputActionMap.FindAction("Fire");
         fireAction.performed += FireAction_performed;
+
+        tiltAction = inputActionMap.FindAction("Move");
+        tiltAction.performed += TiltAction_performed;
+    }
+
+    private void TiltAction_performed(InputAction.CallbackContext obj)
+    {
+        Vector2 tilt = obj.ReadValue<Vector2>();
+
+        if (tilt.x < -0.5)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 5);
+        }
+        else if (tilt.x > 0.5)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, -5);
+        }
+        else if (tilt.y < -0.5)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(-5, 0, 0);
+        }
+        else if (tilt.y > 0.5)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(5, 0, 0);
+        }
     }
 
     private void FireAction_performed(InputAction.CallbackContext obj)
