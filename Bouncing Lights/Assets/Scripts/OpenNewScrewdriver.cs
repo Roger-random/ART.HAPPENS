1ec1293a2da0fe1f8433b580590ebc98c4ff1f82
Roger Cheng <roger.random@outlook.com>
2021-01-29 02:12:18 -0800

    URL hit-testing

--- Bouncing Lights/Assets/Scripts/OpenNewScrewdriver.cs
+++ Bouncing Lights/Assets/Scripts/OpenNewScrewdriver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class OpenNewScrewdriver : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class OpenNewScrewdriver : MonoBehaviour
 
     public InputActionAsset uiControls;
     public InputAction clickAction;
+    public Light spotLight;
+    public float highlightIntensity;
+    public float highlightRate;
+    private float normalLightIntensity;
+    private Collider urlCollider;
 
     public void Awake()
     {
@@ -18,11 +24,19 @@ public class OpenNewScrewdriver : MonoBehaviour
 
         clickAction = inputActionMap.FindAction("Click");
         clickAction.performed += ClickAction_performed;
+
+        urlCollider = GetComponent<Collider>();
+
+        normalLightIntensity = spotLight.intensity;
     }
 
     private void ClickAction_performed(InputAction.CallbackContext obj)
     {
-        OpenNewScrewdriverJS();
+        // Only open https://newscrewdriver.com when mouse click is on URL
+        if (MouseHitTest())
+        {
+            OpenNewScrewdriverJS();
+        }
     }
 
     private void OnEnable()
@@ -33,5 +47,30 @@ public class OpenNewScrewdriver : MonoBehaviour
     private void OnDisable()
     {
         clickAction.Disable();
+    }
+
+    private void Update()
+    {
+        // Highlight when mouse is over URL, but make the spotlight gradually fade from one intensity to another.
+        if (MouseHitTest())
+        {
+            spotLight.intensity = Mathf.MoveTowards(spotLight.intensity, highlightIntensity, highlightRate * Time.deltaTime);
+        }
+        else
+        {
+            spotLight.intensity = Mathf.MoveTowards(spotLight.intensity, normalLightIntensity, highlightRate * Time.deltaTime);
+        }
+    }
+
+    // This feels WAAAAAY more complicated than it needed to be. There's probably a
+    // better way to do this but I don't know it yet.
+    private bool MouseHitTest()
+    {
+        Vector2Control mousev2c = Mouse.current.position;
+        Vector3 mousePosition = new Vector3(mousev2c.x.ReadValue(), mousev2c.y.ReadValue(), 0);
+        Ray mouseRay = Camera.main.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+
+        return urlCollider.Raycast(mouseRay, out hit, 100f);
     }
 }
